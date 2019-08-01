58ecbc27ccfa8cff17f4026015863d578f0f2c4a
mohsen12999 <mohsen1299@gmail.com>
2019-08-01 22:10:23 +0430

    Updates

---
+++ precache-manifest.3428754da0f13f80d1a24d2aa1640a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b773e4498340e39cee603efc038d697",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "0449d41dc886c2306118",
+    "url": "/sudoku/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "de8f7fbe16d47296f871",
+    "url": "/sudoku/static/css/main.8d53d1e0.chunk.css"
+  },
+  {
+    "revision": "0449d41dc886c2306118",
+    "url": "/sudoku/static/js/2.e312c55e.chunk.js"
+  },
+  {
+    "revision": "de8f7fbe16d47296f871",
+    "url": "/sudoku/static/js/main.fa160a24.chunk.js"
+  },
+  {
+    "revision": "2376657f656b447c3d4b",
+    "url": "/sudoku/static/js/runtime~main.e7756cf6.js"
+  }
+]);
