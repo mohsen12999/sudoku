058a1c22025c870d75bf94b8978a7c56f101fa30
mohsen12999 <mohsen1299@gmail.com>
2019-08-04 20:21:58 +0430

    make 9x9

--- src/components/game/gamePage_n2.tsx
+++ src/components/game/gamePage_n2.tsx
@@ -23,7 +23,7 @@ class GamePageN2 extends React.Component<IStateProps, any> {
 
     randomNumber = (n: number) => Math.floor(Math.random() * n);
 
-    makeSquar = (n:number) => {
+    makeSquar = (n: number) => {
         let numbers: Array<number> = new Array(n);
         for (let i: number = 0; i < n; i++) {
             numbers[i] = i;
@@ -40,12 +40,21 @@ class GamePageN2 extends React.Component<IStateProps, any> {
             for (let col: number = 0; col < sqrt; col++) {
                 const thisNum: number = numbers.splice(this.randomNumber(numbers.length), 1)[0];
                 arr[row][col] = thisNum;
-                numbers = numbers.filter(numb=>numb!==thisNum);
+                numbers = numbers.filter(numb => numb !== thisNum);
             }
         }
     }
 
     makeMainSudokuNumber = (n: number) => {
+        // return this._makeMainSudokuNumber(n);
+        do {
+            var arr: number[][]|null = this._makeMainSudokuNumber(n);
+        }while(arr==null);
+
+        return arr;
+    }
+    _makeMainSudokuNumber = (n: number) => {
+
         var arr: Array<Array<number>> = new Array(n);
         for (let i: number = 0; i < arr.length; i++) {
             arr[i] = new Array(n);
@@ -60,116 +69,54 @@ class GamePageN2 extends React.Component<IStateProps, any> {
 
         for (let mainRow: number = 0; mainRow < sqrt; mainRow++) {
             // const element = array[row];
-            for (let MainCol: number = 0; MainCol < sqrt; MainCol++) {
+            for (let mainCol: number = 0; mainCol < sqrt; mainCol++) {
 
-                let thisBoxNumbers = [...numbers];
+                let thisBoxNumbers: Array<number> = [...numbers];
                 // make little squere
                 for (let subRow: number = 0; subRow < sqrt; subRow++) {
                     for (let subCol: number = 0; subCol < sqrt; subCol++) {
 
-                        let validNums = [...thisBoxNumbers]
-                        // Todo: check row
-                        if(mainRow>0){
-                            // validNums.filter
+                        let validNums: Array<number> = [...thisBoxNumbers];
+                        const thisRow: number = mainRow * sqrt + subRow;
+                        const thisCol: number = mainCol * sqrt + subCol;
+                        if (mainRow > 0) {
+                            for (let i: number = 0; i < thisRow; i++) {
+                                validNums = validNums.filter(numb => numb !== arr[i][thisCol]);
+                            }
                         }
-                        // Todo: check col
-                        if(mainRow>0){
-                            // validNums.filter
+                        if (mainCol > 0) {
+                            for (let i: number = 0; i < thisCol; i++) {
+                                validNums = validNums.filter(numb => numb !== arr[thisRow][i]);
+                            }
                         }
-                        if(validNums.length===0) {
+                        if (validNums.length === 0) {
                             // problem happen Do again from beggining
+                            console.log("problem",mainRow,mainCol,thisRow,thisCol);
+                            return null;
                         }
 
-                        const thisNum: number = validNum.splice(this.randomNumber(validNum.length), 1)[0];
-                        const thisRow = mainRow*sqrt+subRow;
-                        const thisCol = mainCol*sqrt+subCol;
+                        const thisNum: number = validNums.splice(this.randomNumber(validNums.length), 1)[0];
 
                         arr[thisRow][thisCol] = thisNum;
-                        thisBoxNumbers = thisBoxNumbers.filter(numb=>numb!==thisNum);
+                        thisBoxNumbers = thisBoxNumbers.filter(numb => numb !== thisNum);
                     }
                 }
-
-
 
             }
         }
-    }
-
-
-    makeNumberSudoku = (n: number) => {
-
-        var arr: Array<Array<number>> = new Array(n);
-        for (let i: number = 0; i < arr.length; i++) {
-            arr[i] = new Array(n);
-        }
-
-        let numbers: Array<number> = new Array(n);
-        for (let i: number = 0; i < n; i++) {
-            numbers[i] = i;
-        }
-
-        let finish: boolean = false;
-
-        while (!finish) {
-            finish = true;
-
-            for (let row: number = 0; row < n; row++) {
-
-                let rowNumbers: Array<number> = [...numbers];
-
-                for (let col: number = 0; col < n; col++) {
-                    let colNumbers: Array<number> = [...rowNumbers];
-
-                    for (let i: number = 0; i < row; i++) {
-                        const element: number = arr[i][col];
-                        colNumbers = colNumbers.filter(value => value !== element);
-                    }
-                    if (colNumbers.length === 0) {
-                        finish = false;
-                        break;
-                    }
-
-                    // check square
-                    let colNumbers2: Array<number> = [...colNumbers];
-                    const sqrt: number = Math.sqrt(n);
-                    const boxRowStart: number = row - (row % sqrt);
-                    const boxColStart: number = col - (col % sqrt);
-                    for (let boxRow: number = boxRowStart; boxRow < boxRowStart + sqrt; boxRow++) {
-                        for (let boxCol: number = boxColStart; boxCol < boxColStart + sqrt; boxCol++) {
-                            const element: number = arr[boxRow][boxCol];
-                            colNumbers2 = colNumbers2.filter(value => value !== element);
-                        }
-                    }
-                    if (colNumbers2.length === 0) {
-                        console.log("check square false",row,col);
-                        // finish = false;
-                        // break;
-                    }
-
-                    const thisNum: number = colNumbers.splice(this.randomNumber(colNumbers.length), 1)[0];
-
-                    if (thisNum === undefined) {
-                        finish = false;
-                        break;
-                    }
-
-                    arr[row][col] = thisNum;
-                    rowNumbers = rowNumbers.filter(value => value !== thisNum);
-                }
-                if (!finish) {
-                    break;
-                }
-            }
-
-        }
-
         return arr;
     }
 
     makeWordSudoku = (wordArray: Array<string>) => {
         const length: number = wordArray.length;
-        const numArray: Array<Array<number>> = this.makeNumberSudoku(length);
-        console.log(numArray);
+        // const numArray: Array<Array<number>> = this.makeNumberSudoku(length);
+        const numArray: Array<Array<number>> = this.makeMainSudokuNumber(length);
+        if(numArray.length===1) {
+            return [[]];
+        }
+
+
+        // console.log(numArray);
 
         var arr: Array<Array<string>> = new Array(length);
         for (let i: number = 0; i < arr.length; i++) {
@@ -191,7 +138,7 @@ class GamePageN2 extends React.Component<IStateProps, any> {
         const arr: Array<Array<string>> = this.makeWordSudoku(words);
         const arrLenght: number = words.length;
         const sqrt: number = Math.sqrt(arrLenght);
-        console.log(arr);
+        // console.log(arr);
         return (
             <div className="container">
                 <h4>this is game page</h4>
@@ -200,22 +147,22 @@ class GamePageN2 extends React.Component<IStateProps, any> {
                     {arr.map((row, index) =>
                         <tr id={"row_" + index} key={"row_" + index}>
                             {row.map((ele, index2) => {
-                                let className:string = "all-td";
-                                if(index===0) {
-                                    className+=" top-border";
-                                } else if(index%sqrt===sqrt-1) {
-                                    className+=" bottom-border";
+                                let className: string = "all-td";
+                                if (index === 0) {
+                                    className += " top-border";
+                                } else if (index % sqrt === sqrt - 1) {
+                                    className += " bottom-border";
                                 }
-                                if(index2===0) {
-                                    className+=" left-border";
-                                } else if(index2%sqrt===sqrt-1) {
-                                    className+=" right-border";
+                                if (index2 === 0) {
+                                    className += " left-border";
+                                } else if (index2 % sqrt === sqrt - 1) {
+                                    className += " right-border";
                                 }
                                 return <td className={className}
-                                            id={"col_"+index + "_" + index2}
-                                            key={"col_"+index + "_" + index2}>
-                                            {ele}
-                                        </td>;
+                                    id={"col_" + index + "_" + index2}
+                                    key={"col_" + index + "_" + index2}>
+                                    {ele}
+                                </td>;
                             }
                             )}
                         </tr>
